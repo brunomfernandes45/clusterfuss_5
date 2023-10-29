85c2a7a64b6dd75b9ee57f0d6af774fd404ef209
Bruno Fernandes (up202108871) <up202108871@up.pt>
2023-10-29 12:03:30 +0000

    More changes to the names of some functions

--- src/utils.pl
+++ src/utils.pl
@@ -3,16 +3,16 @@
 % read_number(-Number)
 % Unifies Number with the number read from the input stream.
 read_number(Number):-
-    read_number_acc(Number, 0).
+    read_number_aux(Number, 0).
 
-% read_number_acc(-Number, +Acc)
+% read_number_aux(-Number, +Acc)
 % Auxiliary predicate for read_number/1.
-read_number_acc(Number, Acc):- 
-    get_code(C),
+read_number_aux(Number, Acc):- 
+    get_code(Code),
     between(48, 57, C), !,
-    Acc1 is 10 * Acc + (C - 48),
-    read_number_acc(Number, Acc1).
-read_number_acc(Number, Number).
+    Acc1 is 10 * Acc + Code - 48,
+    read_number_aux(Number, Acc1).
+read_number_aux(Number, Number).
 
 % symbol(+Color, -Symbol)
 % Unifies Symbol with the symbol corresponding to Color.
