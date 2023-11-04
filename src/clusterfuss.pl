531e36fa0997f20bc350dfdba7e0dfed04790504
Bruno Fernandes (up202108871) <up202108871@up.pt>
2023-11-04 00:58:53 +0000

    Added predicates for testing purposes

--- src/clusterfuss.pl
+++ src/clusterfuss.pl
@@ -15,15 +15,47 @@ play :-
         game(GameState, Mode),
         clear_data.
 
-test :- 
-        board_size(Size),
-        write(Size).
-
 test4 :- 
         asserta(board_size(4)),
         asserta(game_mode(1)),
         initial_state(4, GameState),
-        Move = 'a1'-'b1',
-        move(GameState, Move, NewGameState).
+        game(GameState, 1),
+        clear_data.
+
+test6 :- 
+        asserta(board_size(6)),
+        asserta(game_mode(1)),
+        initial_state(6, GameState),
+        game(GameState, 1),
+        clear_data.
+
+test8 :-
+        asserta(board_size(8)),
+        asserta(game_mode(1)),
+        initial_state(8, GameState),
+        game(GameState, 1),
+        clear_data.
+
+testpc4 :-
+        asserta(board_size(4)),
+        asserta(game_mode(4)),
+        initial_state(4, GameState),
+        game(GameState, 4),
+        clear_data.
+
+testpc6 :-
+        asserta(board_size(6)),
+        asserta(game_mode(4)),
+        initial_state(6, GameState),
+        game(GameState, 4),
+        clear_data.
+
+testpc8 :-
+        asserta(board_size(8)),
+        asserta(game_mode(4)),
+        initial_state(8, GameState),
+        game(GameState, 4),
+        clear_data.
+        
 
 
